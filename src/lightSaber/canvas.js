9b4799bd889505ada2a0af908cf561aa7cdda00c
dachatorn <dachatorn.a@kegroup.co.th>
2022-11-22 11:48:29 +0700

    lightSaber

--- src/lightSaber/canvas.js
+++ src/lightSaber/canvas.js
@@ -15,8 +15,10 @@ const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
+  mouse.x = event.clientX - canvas.width / 2
+  mouse.y = event.clientY - canvas.height / 2
+
+  angle = Math.atan2(mouse.y, mouse.x)
 })
 
 addEventListener('resize', () => {
@@ -26,13 +28,21 @@ addEventListener('resize', () => {
   init()
 })
 
+const center = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+}
+
+let angle = 0
+
 // Objects
-class Object {
-  constructor(x, y, radius, color) {
+class Particle {
+  constructor(x, y, radius, color, distanceFromcenter) {
     this.x = x
     this.y = y
     this.radius = radius
     this.color = color
+    this.distanceFromcenter = distanceFromcenter
   }
 
   draw() {
@@ -43,30 +53,58 @@ class Object {
     c.closePath()
   }
 
-  update() {
+  update(timer) {
     this.draw()
+
+    const { cos, sin } = Math
+    const { distanceFromcenter } = this
+
+    this.x =
+      center.x +
+      distanceFromcenter *
+        cos(-angle) *
+        sin(timer + distanceFromcenter) *
+        cos(timer + distanceFromcenter)
+    this.y =
+      center.y +
+      distanceFromcenter *
+        sin(-angle) *
+        cos(timer + distanceFromcenter) *
+        cos(timer)
   }
 }
 
 // Implementation
-let objects
+let particles
 function init() {
-  objects = []
+  particles = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+  const particleCount = 200
+  const hueIncrement = 360 / particleCount
+  const baseRadius = 3
+  const radiusIncrement = baseRadius / particleCount
+
+  for (let i = 0; i < particleCount; i++) {
+    const x = canvas.width / 2 + i * Math.cos(Math.PI)
+    const y = canvas.height / 2 + i * Math.sin(-Math.PI)
+    particles.push(
+      new Particle(x, y, baseRadius, `hsl(${hueIncrement * i},50%,50%)`, i)
+    )
   }
 }
 
 // Animation Loop
+let timer = 0
 function animate() {
   requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = 'rgba(0,0,0,0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  particles.forEach((particle) => {
+    particle.update(timer)
+  })
+
+  timer += 0.001
 }
 
 init()
